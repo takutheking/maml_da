36b4bff5da4410da927d12a24371d7c2ff8490de
木村拓仁 <takutokimura@kimuratakuhitoshinoMacBook-Pro.local>
2020-06-24 12:54:13 +0900

    added proximal regularization

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import copy
 import math
 import sys
+import time
 
 
 def main():
@@ -16,20 +17,24 @@ def main():
         print("Please type in which data to use: sinusoid || linear")
         sys.exit()
     #maml_hyperparameters
+    torch.autograd.set_detect_anomaly(True)
     meta_lr=0.001
     inner_lr=0.001
     update_num=1
-    meta_epochs=1000
+    meta_epochs=500
     plot_size=meta_epochs//100
-    time=list(range(meta_epochs))
-    time_losses=[0]*meta_epochs
+    start_time=time.time()
     #data
     task_num,data_num=25,5
     data=Data_Generator(num_task=task_num,num_samples_per_task=data_num,task_type=sys.argv[1])
     #model
     model=Predictor()
     optimizer=optim.Adam(model.parameters(),lr=meta_lr)
+    iterations=3
     criterion=nn.MSELoss()
+    mode="maml"
+    #proximal regualization
+    inner_iterations=5
     #da_related
     proposed=True
     u=[0.00001/math.sqrt(k+1) for k in range(meta_epochs)]
@@ -51,17 +56,39 @@ def main():
             sys.stdout.write("\r%d" %epoch)
             sys.stdout.flush()
         for i in range(task_num):
-            loss=F.mse_loss(torch.tensor(outputs[i][:data_num],dtype =torch.float32),model(torch.tensor(inputs[i][:data_num],dtype = torch.float32)))
-            #if create_graph=False, first-order MAML?
-            grad = torch.autograd.grad(loss, model.parameters(),create_graph=True)
-            fast_weights = list(map(lambda p: p[1] - inner_lr * p[0], zip(grad, model.parameters())))
-            losses[i]=F.mse_loss(torch.tensor(outputs[i][data_num:],dtype=torch.float32),model(torch.tensor(inputs[i][data_num:],dtype=torch.float32),vars=fast_weights))
+            if mode=="maml":
+                #regular maml
+                loss=F.mse_loss(torch.tensor(outputs[i][:data_num],dtype =torch.float32),model(torch.tensor(inputs[i][:data_num],dtype = torch.float32)))
+                #if create_graph=False, first-order MAML?
+                grad = torch.autograd.grad(loss, model.parameters(),create_graph=True)
+                fast_weights = list(map(lambda p: p[1] - inner_lr * p[0], zip(grad, model.parameters())))
+                losses[i]=F.mse_loss(torch.tensor(outputs[i][data_num:],dtype=torch.float32),model(torch.tensor(inputs[i][data_num:],dtype=torch.float32),vars=fast_weights))
+        """
+        elif mode=="proximal":
+                temp_model=Predictor()
+                loss=F.mse_loss(torch.tensor(outputs[i][:data_num],dtype =torch.float32),temp_model(torch.tensor(inputs[i][:data_num],dtype = torch.float32)))
+                regularization =torch.tensor(list(map(lambda p: ((p[1] -p[0])**2).sum(), zip(temp_model.parameters(), model.parameters())))).sum()
+                in_optimizer=optim.Adam(temp_model.parameters(),lr=inner_lr)
+                total=loss+regularization
+                for _ in range(inner_iterations):
+                    in_optimizer.zero_grad()
+                    total.backward(retain_graph=True)
+                    in_optimizer.step()
+                losses[i]=total
+
+        else: #dual averaging
+                loss=F.mse_loss(torch.tensor(outputs[i][:data_num],dtype =torch.float32),model(torch.tensor(inputs[i][:data_num],dtype = torch.float32)))
+                #if create_graph=False, first-order MAML?
+                grad = torch.autograd.grad(loss, model.parameters(),create_graph=True)
+                fast_weights = list(map(lambda p: p[1] - inner_lr * p[0], zip(grad, model.parameters())))
+                losses[i]=F.mse_loss(torch.tensor(outputs[i][data_num:],dtype=torch.float32),model(torch.tensor(inputs[i][data_num:],dtype=torch.float32),vars=fast_weights))
+        """
         sum_losses=sum(losses)
-        time_losses[epoch]=sum_losses.detach().numpy().copy().astype(float)
-        if not proposed:
+        for _ in range(iterations):
             optimizer.zero_grad()
-            sum_losses.backward()
+            sum_losses.backward(retain_graph=True)
             optimizer.step()
+        """
         else:
             grad = torch.autograd.grad(sum_losses, model.parameters())
             "initializing"
@@ -78,21 +105,27 @@ def main():
             real_function=1.0/(epoch+1)*linear+u[epoch]*quadratic
             real_function.backward()
             optimizer.step()
+        """
 
     test_task_data_num,test_task_num=20,1
     test_updates=1
+    plot_size=100
     test_inputs, test_outputs, test_amp, test_phase=data.generate()
     x=np.linspace(-5.0,5.0,num=100).reshape(-1,1)
     y_before=model(torch.tensor(x,dtype=torch.float32).reshape(-1,1)).detach().numpy()
     plt.plot(x,y_before,label="before finetuning")
-    optimizer=optim.Adam(model.parameters(),lr=3*meta_lr)
+    optimizer=optim.Adam(model.parameters(),lr=meta_lr)
     for i in range(test_updates):
         optimizer.zero_grad()
-        loss=criterion(torch.tensor(test_outputs[0][:test_task_data_num],dtype=torch.float32),model(torch.tensor(test_inputs[0][:test_task_data_num],dtype=torch.float32)))
-        loss.backward()
-        optimizer.step()
+        for _ in range(iterations):
+            loss=criterion(torch.tensor(test_outputs[0][:test_task_data_num],dtype=torch.float32),model(torch.tensor(test_inputs[0][:test_task_data_num],dtype=torch.float32)))
+            loss.backward()
+            optimizer.step()
     loss=criterion(torch.tensor(test_outputs[0][test_task_data_num:],dtype=torch.float32),model.forward(torch.tensor(test_inputs[0][test_task_data_num:],dtype=torch.float32)))
     y_predict=model(torch.tensor(x,dtype=torch.float32).reshape(-1,1)).detach().numpy()
+    total_se=np.square(y_predict-np.linspace(-5.0,5.0,num=plot_size).reshape(-1,1)).mean()
+    print("\nThe execution time:",time.time()-start_time)
+    print("\nThe total squared error",total_se)
     plt.plot(x,test_phase[0]*np.sin(x-test_amp[0]),label="oracle")
     plt.plot(x,y_predict,label="after fine-tuning")
     plt.legend()
